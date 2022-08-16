3314d666a3f74d2af253ce122e9b29fd96cb1eab
sourdough <jackie.chan.shopee101@gmail.com>
2022-08-16 15:47:20 +0800

    Filter, validate, & store numeric input

--- script.js
+++ script.js
@@ -10,4 +10,39 @@ function operate(operator, num1, num2) {
     return obj[operator];
 }
 
-console.log(operate('+', 1, 2));
+const screen = document.querySelector('.top');
+let numResult = 0;
+let temp = '';
+let num2 = '';
+let operator = '';
+screen.textContent = numResult;
+
+document.addEventListener('click', main);
+
+function main(e) {
+    const input = e.target;
+    if (input.nodeName !== "BUTTON") return;
+    getNumber(input);
+    getOperator(input);
+}
+
+function getOperator(userInput) {
+    if (userInput.className === 'symbols' && !userInput.id) {
+        
+    }
+}
+
+function getNumber(userInput) {
+    if (userInput.className !== 'num') {
+        return;
+     } else {
+        if (screen.textContent === '0' && userInput.textContent === '0') return;
+        if (screen.textContent.includes('.') && userInput.textContent === '.') return;
+        if (screen.textContent === '0' && userInput.textContent !== '0') {
+            screen.textContent = userInput.textContent;
+        } else {
+            screen.textContent += userInput.textContent;
+        }
+     }
+     temp = screen.textContent;
+}

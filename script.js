9ac0a9d7ba58b11219fb6624e4221c2279464fd3
sourdough <jackie.chan.shopee101@gmail.com>
2022-08-25 14:27:55 +0800

    Perform operations & working clear button
    Able to string together several operations and get the right answer,
    with each pair of numbers being evaluated at a time. For example,
    12 + 7 - 5 * 3 = should yield 42.
    
    Still need to do the "=" function.

--- script.js
+++ script.js
@@ -1,48 +1,93 @@
 "use strict"
 
-function operate(operator, num1, num2) {    
-    let obj =  {
-        '+' : num1 + num2,
-        '-' : num1 - num2,
-        '/' : num1 / num2,
-        '*' : num1 * num2,
-    };
-    return obj[operator];
-}
-
 const screen = document.querySelector('.top');
-let numResult = 0;
-let temp = '';
+let numResult = '';
+let num1 = '';
 let num2 = '';
-let operator = '';
-screen.textContent = numResult;
+let activeOperation = '';
+let nextOperation = '';
+let operatorActive = false;
+//screen.textContent = numResult;
 
 document.addEventListener('click', main);
 
 function main(e) {
     const input = e.target;
     if (input.nodeName !== "BUTTON") return;
-    getNumber(input);
-    getOperator(input);
+    listenForClear(input);
+    listenForNumber(input);
+    listenForOperator(input);
+    //listenForEquals(input);
 }
+/*
+function listenForEquals(userInput) {
+    let result = 0;
+    if (userInput.id) {
+        if (activeOperation, num2, num1) {
+            numResult = operate(activeOperation, +num2, +num1);
+        }
+    }
+    screen.textContent = numResult;
+}
+*/
+function listenForOperator(userInput) {
+    let result = 0;
+    
+    if (userInput.className === 'symbols') {
+        nextOperation = userInput.textContent;
+        operatorActive = true;
 
-function getOperator(userInput) {
-    if (userInput.className === 'symbols' && !userInput.id) {
-        
+        if (!num2) {
+            num2 = num1;
+            num1 = '';
+            activeOperation = nextOperation;
+        } else if (num1 && num2 && nextOperation) {
+            numResult = operate(activeOperation, num1, num2);
+            screen.textContent = numResult;
+            num2 = numResult;
+            numResult = ''
+            num1 = '';
+            activeOperation = nextOperation;
+        }         
+
     }
 }
 
-function getNumber(userInput) {
-    if (userInput.className !== 'num') {
-        return;
-     } else {
+function listenForNumber(userInput) {
+    if (userInput.className !== 'num') return;
+    if (operatorActive) {
+        screen.textContent = userInput.textContent;
+        operatorActive = false;
+    } else {
         if (screen.textContent === '0' && userInput.textContent === '0') return;
-        if (screen.textContent.includes('.') && userInput.textContent === '.') return;
-        if (screen.textContent === '0' && userInput.textContent !== '0') {
+        if (screen.textContent.includes('.') && userInput.textContent === '.' ) return;
+        if (screen.textContent === '0' && userInput.textContent !== '0' ) {
             screen.textContent = userInput.textContent;
         } else {
             screen.textContent += userInput.textContent;
         }
      }
-     temp = screen.textContent;
+     num1 = screen.textContent;
+}
+
+function listenForClear(userInput) {
+    if (userInput.className === 'btn-clear') {
+        let numResult = '';
+        let num1 = '';
+        let num2 = '';
+        let activeOperation = '';
+        let nextOperation = '';
+        let operatorActive = false;
+        screen.textContent = '0';
+    }
+}
+
+function operate(operator, num1, num2) {    
+    let obj =  {
+        '+' : +num2 + +num1,
+        '-' : +num2 - +num1,
+        '/' : +num2 / +num1,
+        'x' : +num2 * +num1,
+    };
+    return obj[operator];
 }

722e1b5120507d8951a80647a073e142084b286c
sourdough <jackie.chan.shopee101@gmail.com>
2022-09-08 14:07:06 +0800

    Accept keyboard entry

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let equalActive = false;
 const screen = document.querySelector('.top');
 
 const numberKeys = document.querySelectorAll('.num');
-numberKeys.forEach(key => key.addEventListener('click', getNumber));
+numberKeys.forEach(key => key.addEventListener('click', e => getNumber(e.target.textContent)));
 function getNumber(e) {
-    let selectedNumber = e.target.textContent;
+    let selectedNumber = e;
     if (equalActive) {
         previousNumber = '';
         equalActive = false;
@@ -23,30 +23,35 @@ function getNumber(e) {
 
 const periodKey = document.querySelector('.period');
 periodKey.addEventListener('click', getPeriod);
-function getPeriod(e) {
-    if (presentNumber.includes('.')) return;
-    presentNumber += '.';
-    screen.textContent = presentNumber;
-}
+function getPeriod() {
+    if (!presentNumber.includes('.')) {
+        presentNumber += '.';
+        screen.textContent = presentNumber;
+    }
+};
 
 const operationKeys = document.querySelectorAll('.symbols');
-operationKeys.forEach(key => key.addEventListener('click', getOperation));
+operationKeys.forEach(key => key.addEventListener('click', e => getOperation(e.target.textContent)));
 function getOperation(e) {
     if (previousNumber && presentNumber && operation) {
         previousNumber = operate(previousNumber, presentNumber, operation);
         if (previousNumber === Infinity) {
             alert("Can't divide by zero");
             clearAll();
+        } else if (previousNumber.toString().length > 10) {
+            screen.textContent = previousNumber.toExponential(3);
+            operation = e;
+            presentNumber = '';
         } else {
             screen.textContent = previousNumber;
-            operation = e.target.textContent;
+            operation = e;
             presentNumber = '';
         }
     } else if (equalActive) {
-        operation = e.target.textContent;
+        operation = e;
         equalActive = false;
     } else {
-        operation = e.target.textContent;
+        operation = e;
         previousNumber = presentNumber;
         presentNumber = '';
     }
@@ -54,12 +59,17 @@ function getOperation(e) {
 
 const equalKey = document.querySelector('.equal');
 equalKey.addEventListener('click', getEqual);
-function getEqual(e) {
+function getEqual() {
     if (previousNumber && presentNumber && operation) {
         previousNumber = operate(previousNumber, presentNumber, operation);
         if (previousNumber === Infinity) {
             alert("Can't divide by zero");
             clearAll();
+        } else if (previousNumber.toString().length > 10) {
+            screen.textContent = previousNumber.toExponential(3);
+            operation = '';
+            presentNumber = '';
+            equalActive = true;
         } else {
             screen.textContent = previousNumber;
             operation = '';
@@ -79,12 +89,29 @@ function clearAll() {
     screen.textContent = '0';
 }
 
+window.addEventListener('keydown', keyboardEntry);
+function keyboardEntry(e) {
+    e.preventDefault();
+    if (+e.key >=0 || +e.key<=9) {
+        getNumber(e.key);
+    } else if (e.key === '.') {
+        getPeriod();
+    } else if (e.key ==='+' || e.key ==='-' || e.key ==='/') {
+        getOperation(e.key);
+    } else if (e.key === '*') {
+        getOperation('x')
+    } else if (e.key === 'Enter') {
+        getEqual();
+    }
+}
+
 function operate(firstNumber, secondNumber, operator) {
     let obj =  {
         '+' : +firstNumber + +secondNumber,
         '-' : +firstNumber - +secondNumber,
         '/' : +firstNumber / +secondNumber,
         'x' : +firstNumber * +secondNumber,
+        '*' : +firstNumber * +secondNumber,
     };
     return obj[operator];
 }

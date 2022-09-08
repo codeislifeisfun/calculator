5c676599624b648257385e67c6e60edf22acc8a0
sourdough <jackie.chan.shopee101@gmail.com>
2022-09-08 11:50:01 +0800

    Catch divide by zero

--- script.js
+++ script.js
@@ -3,14 +3,22 @@
 let presentNumber = '';
 let previousNumber = '';
 let operation = '';
-let screen = document.querySelector('.top');
+let equalActive = false;
+const screen = document.querySelector('.top');
 
 const numberKeys = document.querySelectorAll('.num');
 numberKeys.forEach(key => key.addEventListener('click', getNumber));
 function getNumber(e) {
     let selectedNumber = e.target.textContent;
-    presentNumber += selectedNumber;
-    screen.textContent = presentNumber;
+    if (equalActive) {
+        previousNumber = '';
+        equalActive = false;
+        presentNumber += selectedNumber;
+        screen.textContent = presentNumber;
+    } else {
+        presentNumber += selectedNumber;
+        screen.textContent = presentNumber;
+    }
 }
 
 const periodKey = document.querySelector('.period');
@@ -26,9 +34,17 @@ operationKeys.forEach(key => key.addEventListener('click', getOperation));
 function getOperation(e) {
     if (previousNumber && presentNumber && operation) {
         previousNumber = operate(previousNumber, presentNumber, operation);
-        screen.textContent = previousNumber;
+        if (previousNumber === Infinity) {
+            alert("Can't divide by zero");
+            clearAll();
+        } else {
+            screen.textContent = previousNumber;
+            operation = e.target.textContent;
+            presentNumber = '';
+        }
+    } else if (equalActive) {
         operation = e.target.textContent;
-        presentNumber = '';
+        equalActive = false;
     } else {
         operation = e.target.textContent;
         previousNumber = presentNumber;
@@ -36,7 +52,34 @@ function getOperation(e) {
     }
 }
 
-function operate(firstNumber, secondNumber, operator) {    
+const equalKey = document.querySelector('.equal');
+equalKey.addEventListener('click', getEqual);
+function getEqual(e) {
+    if (previousNumber && presentNumber && operation) {
+        previousNumber = operate(previousNumber, presentNumber, operation);
+        if (previousNumber === Infinity) {
+            alert("Can't divide by zero");
+            clearAll();
+        } else {
+            screen.textContent = previousNumber;
+            operation = '';
+            presentNumber = '';
+            equalActive = true;
+        }
+    }
+}
+
+const clearKey = document.querySelector('.btn-clear');
+clearKey.addEventListener('click', clearAll);
+function clearAll() {
+    presentNumber = '';
+    previousNumber = '';
+    operation = '';
+    equalActive = false;
+    screen.textContent = '0';
+}
+
+function operate(firstNumber, secondNumber, operator) {
     let obj =  {
         '+' : +firstNumber + +secondNumber,
         '-' : +firstNumber - +secondNumber,

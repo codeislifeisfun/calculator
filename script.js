a5d102015a40bec4bfd0f2bfd58a4fc32378b10e
sourdough <jackie.chan.shopee101@gmail.com>
2022-09-08 09:04:55 +0800

    Create event listeners per button group

--- script.js
+++ script.js
@@ -1,90 +1,47 @@
 "use strict"
 
-const screen = document.querySelector('.top');
-let numResult = '';
-let num1 = '';
-let num2 = '';
-let activeOperation = '';
-let nextOperation = '';
-let operatorActive = false;
-//screen.textContent = numResult;
+let presentNumber = '';
+let previousNumber = '';
+let operation = '';
+let screen = document.querySelector('.top');
 
-document.addEventListener('click', main);
-
-function main(e) {
-    const input = e.target;
-    if (input.nodeName !== "BUTTON") return;
-    listenForClear(input);
-    listenForNumber(input);
-    listenForOperator(input);
-    //listenForEquals(input);
-}
-/*
-function listenForEquals(userInput) {
-    let result = 0;
-    if (userInput.id) {
-        if (activeOperation, num2, num1) {
-            numResult = operate(activeOperation, +num2, +num1);
-        }
-    }
-    screen.textContent = numResult;
-}
-*/
-function listenForOperator(userInput) {    
-    if (userInput.className === 'symbols') {
-        nextOperation = userInput.textContent;
-        operatorActive = true;
-
-        if (!num2) {
-            num2 = num1;
-            num1 = '';
-            activeOperation = nextOperation;
-        } else if (num1 && num2 && nextOperation) {
-            numResult = operate(activeOperation, num1, num2);
-            screen.textContent = numResult;
-            num2 = numResult;
-            numResult = ''
-            num1 = '';
-            activeOperation = nextOperation;
-        }
-    }
+const numberKeys = document.querySelectorAll('.num');
+numberKeys.forEach(key => key.addEventListener('click', getNumber));
+function getNumber(e) {
+    let selectedNumber = e.target.textContent;
+    presentNumber += selectedNumber;
+    screen.textContent = presentNumber;
 }
 
-function listenForNumber(userInput) {
-    if (userInput.className !== 'num') return;
-    if (operatorActive) {
-        screen.textContent = userInput.textContent;
-        operatorActive = false;
+const periodKey = document.querySelector('.period');
+periodKey.addEventListener('click', getPeriod);
+function getPeriod(e) {
+    if (presentNumber.includes('.')) return;
+    presentNumber += '.';
+    screen.textContent = presentNumber;
+}
+
+const operationKeys = document.querySelectorAll('.symbols');
+operationKeys.forEach(key => key.addEventListener('click', getOperation));
+function getOperation(e) {
+    if (previousNumber && presentNumber && operation) {
+        previousNumber = operate(previousNumber, presentNumber, operation);
+        screen.textContent = previousNumber;
+        operation = e.target.textContent;
+        presentNumber = '';
     } else {
-        if (screen.textContent === '0' && userInput.textContent === '0') return;
-        if (screen.textContent.includes('.') && userInput.textContent === '.' ) return;
-        if (screen.textContent === '0' && userInput.textContent !== '0' ) {
-            screen.textContent = userInput.textContent;
-        } else {
-            screen.textContent += userInput.textContent;
-        }
-     }
-     num1 = screen.textContent;
-}
-
-function listenForClear(userInput) {
-    if (userInput.className === 'btn-clear') {
-        numResult = '';
-        num1 = '';
-        num2 = '';
-        activeOperation = '';
-        nextOperation = '';
-        operatorActive = false;
-        screen.textContent = '0';
+        operation = e.target.textContent;
+        previousNumber = presentNumber;
+        presentNumber = '';
     }
 }
 
-function operate(operator, num1, num2) {    
+function operate(firstNumber, secondNumber, operator) {    
     let obj =  {
-        '+' : +num2 + +num1,
-        '-' : +num2 - +num1,
-        '/' : +num2 / +num1,
-        'x' : +num2 * +num1,
+        '+' : +firstNumber + +secondNumber,
+        '-' : +firstNumber - +secondNumber,
+        '/' : +firstNumber / +secondNumber,
+        'x' : +firstNumber * +secondNumber,
     };
     return obj[operator];
 }
